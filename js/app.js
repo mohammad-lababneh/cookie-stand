13b0680097d1138c2922fef2eafc0cbbb3b5f195
mohammad-lababneh <mohammadlababneh96@gmail.com>
2021-05-10 10:26:07 +0300

    tree

--- js/app.js
+++ js/app.js
@@ -19,8 +19,6 @@ function city(cityname, max, min, avg) {
     cities.push(this);
 
 }
-
-
 city.prototype.randonNumber = function () {
 
     for (let i = 0; i < 14; i++) {
@@ -28,8 +26,6 @@ city.prototype.randonNumber = function () {
 
     }
 }
-
-
 city.prototype.randomAvg = function () {
 
     for (let i = 0; i < 14; i++) {
@@ -38,17 +34,13 @@ city.prototype.randomAvg = function () {
         this.Total += this.salesPerHour[i];
 
     }
+    
 }
-
-
 let Seattle = new city('Seattle', 65, 23, 6.3);
 let Tokyo = new city('Tokyo', 24, 3, 1.2);
 let Dubai = new city('Dubai', 38, 11, 3.7);
 let Paris = new city('Paris', 38, 20, 2.3);
 let Lima = new city('Lima', 16, 2, 4.6);
-
-
-
 
 function hoursDay() {
 
@@ -79,24 +71,15 @@ th3.textContent = 'Daily Location Total';
 
 city.prototype.render = function () {
 
-    // let container = document.getElementById('container');
-    // let table = document.createElement('table');
-    // container.appendChild(table);
-
     let tr1 = document.createElement('tr');
     table.appendChild(tr1);
-
     let td1 = document.createElement('td');
     tr1.appendChild(td1);
     td1.textContent = this.cityname;
 
-
-
     for (let i = 0; i < 14; i++) {
 
         let td3 = document.createElement('td');
-
-
         tr1.appendChild(td3);
         td3.textContent = this.salesPerHour[i];
 
@@ -107,25 +90,15 @@ th.textContent = this.Total;
 }
 
  function footer (){
-
-//     // let container = document.getElementById('container');
-//     // let table = document.createElement('table');
-//     // container.appendChild(table);
-
     let tr1 = document.createElement('tr');
     table.appendChild(tr1);
-
-
     let th1 = document.createElement('th');
     tr1.appendChild(th1);
     th1.textContent = 'Totals';
-    
     let totalOfTotal = 0;
     for (let i = 0; i < HOURES.length; i++) {
         let totalPerHour = 0;
     for (let j = 0; j < cities.length; j++) {
-        // let th2 = document.createElement('td');
-        // tr1.appendChild(th2);
         totalPerHour +=  cities[j].salesPerHour[i];
     totalOfTotal += cities[j].salesPerHour[i];
     }
@@ -133,13 +106,9 @@ th.textContent = this.Total;
     tr1.appendChild(th2);
         th2.textContent = totalPerHour;
      }
-    // console.log(   totalPerHour);
-  
-
     let th3 = document.createElement('th');
     tr1.appendChild(th3);
     th3.textContent = totalOfTotal;
-  
     }
 
 
